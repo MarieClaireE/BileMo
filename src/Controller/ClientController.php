55bc48c1091dc245ce7d2f47f86001f41e679e04
MarieClaireE <mcemma.974@gmail.com>
2023-03-16 10:28:38 +0100

    codacy

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -18,12 +18,13 @@
 
 class ClientController extends AbstractController
 	{
+
 				public const CACHE_KEY_GETALLCUSTOMERS = "getAllCustomers";
 
 				public function getRepository(): ClientRepository
-	{
-		return $this->em->getRepository(Client::class);
-	}
+				{
+					return $this->em->getRepository(Client::class);
+				}
 
 				/**
 				 * @return JsonResponse
@@ -31,28 +32,33 @@ class ClientController extends AbstractController
 				 */
 				#[Route('api/clients/', name:'list_clients', methods:['GET'])]
 				public function getClientList(): JsonResponse
-					{
-						$customerList = $this->cachePool->get (self::CACHE_KEY_GETALLCUSTOMERS, function (ItemInterface $item) {
-							$item->tag ('customersCache');
-							return $this->getRepository ()->findAll();
-						});
+				{
 
-						$jsonCustomerList = $this->serializer->serialize($customerList, 'json', ['groups' => 'getClients', 'getProduits', 'getUtilisateurs']);
-						return new JsonResponse($jsonCustomerList, Response::HTTP_OK, [], true);
-					}
+					$customerList = $this->cachePool->get (self::CACHE_KEY_GETALLCUSTOMERS, function (ItemInterface $item) {
+						$item->tag ('customersCache');
+						return $this->getRepository ()->findAll();
+					});
+
+					$jsonCustomerList = $this->serializer->serialize($customerList, 'json', ['groups' => 'getClients', 'getProduits', 'getUtilisateurs']);
+					return new JsonResponse($jsonCustomerList, Response::HTTP_OK, [], true);
+
+				}
 
 				#[Route('api/clients/{id}', name:'details_clients', methods:['GET'])]
 				public function getDetailsClient(Client $client): JsonResponse
-		{
-			$jsonCustomer = $this->serializer->serialize($client, 'json', ['groups' => 'getClients', 'getProduits', 'getUtilisateurs']);
+				{
 
-			return new JsonResponse($jsonCustomer, Response::HTTP_OK, ['accept' => 'json'], true);
-		}
+					$jsonCustomer = $this->serializer->serialize($client, 'json', ['groups' => 'getClients', 'getProduits', 'getUtilisateurs']);
+
+					return new JsonResponse($jsonCustomer, Response::HTTP_OK, ['accept' => 'json'], true);
+
+				}
 
 				#[Route('api/clients/', name:'delete_client', methods:['DELETE'])]
 				#[IsGranted('ROLE_ADMIN', message:'Vous n\'avez pas les droits requis pour accéder à la liste des clients')]
 				public function deleteClient(): JsonResponse
 				{
+
 					$response = '';
 					$client = $this->getUser();
 					if(is_null($client)) {
@@ -63,7 +69,7 @@ class ClientController extends AbstractController
 						$this->em->flush();
 						$response = new JsonResponse(['success' => 'Client supprimé avec succès'], Response::HTTP_OK);
 					}
+				return $response;
 
-			return $response;
-		}
+			}
 	}
